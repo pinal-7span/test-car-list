d69dd6e40a024652f9e0d62d590e8e83b6c7ce58
Pinal Tilva <pinal@7span.com>
2023-01-20 20:01:33 +0530

    build component for fecting car list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import CarList from "./component/list";
+import carList from "./data/dataJSON.json";
 
 function App() {
   return (
     <div className="App">
-      <h2>My app</h2>
+      <div>
+        <CarList label="Maker" type="Make" detail="Company of the vehicle" />
+        <CarList label="Model" type="Model" detail="Car Model" />
+        <CarList
+          label="Vehicle Class"
+          type="Vehicle Class"
+          detail="Class of vahicle depending on their utility, capacity and weight"
+        />
+      </div>
     </div>
   );
 }
